b52fbecc744507a536405df799f1fc2f80498e8a
MANOJ K <24f2004663@ds.study.iitm.ac.in>
2025-05-24 10:29:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function scrollLeft() {
-  document.getElementById('eventCards').scrollBy({ left: -300, behavior: 'smooth' });
-}
-
-function scrollRight() {
-  document.getElementById('eventCards').scrollBy({ left: 300, behavior: 'smooth' });
-}
